f74db9f3d5596f07d5042ac2f3fb3e1fc5845983
Karl Gunnar <karl.gunnar@gmail.com>
2015-02-20 14:40:38 +0100

    lagt til bogus

--- Relestyring/include/adc_setup.h
+++ Relestyring/include/adc_setup.h
@@ -5,11 +5,11 @@
  *      Author: arch
  */
 
-#ifndef INCLUDE_ADC_SETUP_H_
-#define INCLUDE_ADC_SETUP_H_
+#ifndef RELESTYRING_INCLUDE_ADC_SETUP_H_
+#define RELESTYRING_INCLUDE_ADC_SETUP_H_
 
 void ADC_Config(void);
 
 int adc_convert(void);
 
-#endif /* INCLUDE_ADC_SETUP_H_ */
+#endif /* RELESTYRING_INCLUDE_ADC_SETUP_H_ */
