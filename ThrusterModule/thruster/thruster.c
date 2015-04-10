a815fda4dbf79a7b5871aad6a40b0c1d5b112675
Tor Morten Finnesand <tm.finnesand@gmail.com>
2015-04-10 15:18:09 +0200

    DETTE er laget av T.johnsen AKA b.bob
    kan nå builde

--- ThrusterModule/thruster/thruster.c
+++ ThrusterModule/thruster/thruster.c
@@ -1,14 +1,14 @@
 #include "ch.h"
 #include "hal.h"
 #include "thruster.h"
-
+#include <math.h>
 static struct ThValues receiveData(void);
 
 struct ThValues{
-	uint16_t X;
-	uint16_t Y;
-	uint16_t Z;
-	uint16_t ROT;
+	int16_t X;
+	int16_t Y;
+	int16_t Z;
+	int16_t ROT;
 };
 
 static struct ThValues receiveData() {
@@ -37,20 +37,21 @@ msg_t thruster_thread(void *p){
 	struct ThValues verdi;
 
 /* Definerer korrigerings variabler */
-uint16_t verdiB_X;
-uint16_t verdiB_Y;
-uint16_t verdiB_Z;
-uint16_t korrigert;
-uint16_t M1, M2, M3, M4;
+int16_t verdiB_X;
+int16_t verdiB_Y;
+int16_t verdiB_Z;
+int16_t korrigert =100;
+int16_t M1, M2, M3, M4;
+int16_t M1velger, M2velger, M3velger, M4velger;
 double r;
 double theta;
 double thetaInn;
 double M1Foer_kor, M2Foer_kor, M3Foer_kor, M4Foer_kor;
 
 double skalering =1 ;
-int inversPropellKar[2] [10] =
- {1, 2,3,4,5,6,7,8,9,10}, // Fremover Thrust retning (pluss)
- {1, 2,3,4,5,6,7,8,9,10}  // bakover   Thrust retning (negativ)
+int inversPropellKar[2] [10] ={
+ {1,2,3,4,5,6,7,8,9,10}, // Fremover Thrust retning (pluss)
+ {1,3,3,4,5,6,7,8,9,10}};  // bakover   Thrust retning (negativ)
 
 
 
@@ -76,12 +77,12 @@ int inversPropellKar[2] [10] =
 		
 		/* Gjør bergenet verdier om til PWM verdi for negativt paadrag */
 		if(verdiB_Z < 0){
-			verdiB_Z = 750 - abs(verdiB_Z) * 2.5;
+			verdiB_Z = 750 + (verdiB_Z * 2.5);
 		}
 
 		/* Gjør bergenet verdier om til PWM verdi for de resterende paadragene */
-		else(verdiB_Z => 0){
-			verdiB_Z = 750 + abs(verdiB_Z) * 2.5;
+		else/* if (verdiB_Z >= 0)*/{
+			verdiB_Z = 750 + (verdiB_Z * 2.5);
 		}
 
 
@@ -100,29 +101,26 @@ int inversPropellKar[2] [10] =
 		
 
 		/* PROGRAM FOR HORISONTAL THRUSTERS */ 
-8
+
 		r = (double) sqrt(verdi.X^2*verdi.Y^2)/2;
-		if 100/2 < r{
+		if (100/2 < r) {
 			r = 100/2;
 		}
 
 		theta = 0;
-		thetaInn = (double) atand(verdi.Y/verdi.X);
+		thetaInn = (double) atan(verdi.Y/verdi.X);
+		thetaInn = thetaInn *(180/M_PI); //rad => deg
 
-		if verdi.X>0 && verdi.Y>0{ /* I kvadrant */
+		if (verdi.X>0 && verdi.Y>0){ /* I kvadrant */
 			theta = thetaInn;
-		}else verdi.X<0 && verdi.Y>0{ /* II kvadrant */
+		}else if (verdi.X<0 && verdi.Y>0){ /* II kvadrant */
 			theta = thetaInn + 180; 
-		}else verdi.X<0 && verdi.Y<0{ /* III kvadrant */
+		}else if (verdi.X<0 && verdi.Y<0){ /* III kvadrant */
 			theta = thetaInn - 180;
-		}else verdi.X>0 && verdi.Y<0{ /* IV kvadrant */
+		}else if (verdi.X>0 && verdi.Y<0){ /* IV kvadrant */
 			theta = thetaInn;
 		}
 		
-		/*______linearisering av propell karraktestikk_______*/
-
-
-
 
 
 		
@@ -133,46 +131,43 @@ int inversPropellKar[2] [10] =
 		M4Foer_kor = -M1Foer_kor ;
 
 
-		/* Konverterer fra double til uint */
-		M1 = (uint16_t) M1Foer_kor;
-		M2 = (uint16_t) M2Foer_kor;
-		M3 = (uint16_t) M3Foer_kor;
-		M4 = (uint16_t) M4Foer_kor;
+		/* Konverterer fra double til int for å få en verdi som kan  */
+		M1velger = (int16_t) M1Foer_kor;
+		M2velger = (int16_t) M2Foer_kor;
+		M3velger = (int16_t) M3Foer_kor;
+		M4velger = (int16_t) M4Foer_kor;
 
+			/*______linearisering av propell karraktestikk_______*/
+		
 		/* korrigerer for  ulinært forhol mellom thrust og pådrag denne vil ikke fungere ved negativt pådrag
 		, dersom karraktestikk ikke er lik for frem/revers må det legges til to matriser, en for hver retning  */
-		if (M1>= 0)
-			{ M1 = M1 * inversPropellKar[1][M1] } ;
-		else { M1 = M1 * inversPropellKar[2][M1] };
+		if (M1Foer_kor>= 0)
+			{ M1Foer_kor = M1Foer_kor * inversPropellKar[1][M1velger]  ;}
+		else { M1Foer_kor = M1Foer_kor * inversPropellKar[2][M1velger] ;}
 
 		
-		if (M2>= 0)
-			{ M2 = M2 * inversPropellKar[2][M1] } ;
-		else { M2 = M2 * inversPropellKar[2][M2] };
+		if (M2Foer_kor>= 0)
+			{ M2Foer_kor = M2Foer_kor * inversPropellKar[2][M1velger]  ;}
+		else { M2Foer_kor = M2Foer_kor * inversPropellKar[2][M2velger] ;}
 
 
-		if (M3>= 0)
-			{ M3 = M3 * inversPropellKar[1][M3] } ;
-		else { M3 = M3 * inversPropellKar[2][M3] };
+		if (M3Foer_kor>= 0)
+			{ M3Foer_kor = M3Foer_kor * inversPropellKar[1][M3velger]  ;}
+		else { M3Foer_kor = M3Foer_kor * inversPropellKar[2][M3velger] ;}
 
 		
-		if (M4>= 0)
-			{ M4 = M4 * inversPropellKar[1][M4] } ;
-		else { M4 = M4 * inversPropellKar[2][M4] };
+		if (M4Foer_kor>= 0)
+			{ M4Foer_kor = M4Foer_kor * inversPropellKar[1][M4velger]  ;} 
+		else { M4Foer_kor = M4Foer_kor * inversPropellKar[2][M4velger] ;}
 
 
 
 
-
-
-
-
-		/* Psaudo, korrigering av pådrag med invers thruster kar.  
-		M1= M1* lesDataFraGraf(M1);
-		Lag for M2-M3;
-		
-
-		/* Følgende funksjon kan benyttes te å sette pådrag til trustere
+		M1 = ((int16_t) M1Foer_kor)*100;
+		M2 = ((int16_t) M2Foer_kor)*100;
+		M3 = ((int16_t) M3Foer_kor)*100;
+		M4 = ((int16_t) M4Foer_kor)*100;
+		 /*Følgende funksjon kan benyttes for å sette pådrag til trustere
 	 	* 
 	 	* pwmEnableChannel(&PWMDx, kanal, PWM_PERCENTAGE_TO_WIDTH(&PWMDx, duty cycle))
 	 	* 
@@ -181,9 +176,9 @@ int inversPropellKar[2] [10] =
 	 	* dutyCycle kan ha verdi mellom 0 - 10000 hvor 10000 er 100%
 	 	* Eksempel:
 	 	* 
-	 	* pwmEnableChannel(&PWMD1, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 2000)); */
+	 	* pwmEnableChannel(&PWMD1, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 2000)); 
 
-	 	/* Eksempel: hente ut verdi
+	 	 Eksempel: hente ut verdi
 	 	verdi.X */
 	}	 
 	
