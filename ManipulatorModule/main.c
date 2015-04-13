acef103c5140363e01e3198156685376e36007f5
Tor Morten Finnesand <tm.finnesand@gmail.com>
2015-04-13 15:29:52 +0200

    Started work on adding CAN to Manip module
    The code base needs cleanup, this is the last commit
    before fast forwarding

--- ManipulatorModule/main.c
+++ ManipulatorModule/main.c
@@ -16,23 +16,8 @@
 
 #include "ch.h"
 #include "hal.h"
-uint16_t i = 1;         // Lager teller variabel i    
-uint16_t j = 1;         // Lager teller variabel j 
-int VerdiJoy;
-int Joyakse1;
-int Joyakse2;
-int Joyakse3;
-int Joyakse4;
-int Joyakse5;
+#include "manip/manip.h"
 
-
-static void pwmteller(PWMDriver *pwmp){
- (void)pwmp;
- if (palReadPad(GPIOA,GPIOA_BUTTON))
- {
- 
- }
-}
 /*
  * PWM configuration structure.
  * Cyclic callback enabled, channels 1 and 4 enabled without callbacks,
@@ -81,75 +66,12 @@ int main(void) {
   pwmStart(&PWMD8, &pwmcfg);    // Starter Timer 8
 
 
-  while(TRUE) {     //Sett inn styrespak
-    pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000)); //PA8
-    if(VerdiJoy>=0)
-    {
-      if(Joyakse1)
-      {
-        pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000)); //PA8
-      }
-      else if(Joyakse2)
-      {
-        pwmEnableChannel(&PWMD2, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD2, 1000)); //PA15
-      }
-      else if(Joyakse3)
-      {
-        pwmEnableChannel(&PWMD3, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD3, 1000)); //PA15
-      }
-      else if(Joyakse4)
-      {
-        pwmEnableChannel(&PWMD4, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD4, 1000)); //PA15
-      }
-      else if(Joyakse5)
-      {
-        pwmEnableChannel(&PWMD8, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD8, 1000)); //PA15
-      }
-      
-       
-      // pwmChangePeriod(&PWMD4, 10);
-      // chThdSleepMilliseconds(3000);
-      // pwmDisableChannel(&PWMD4, 1);
-      // pwmEnableChannel(&PWMD3, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD3, 1000));
-    }
-    else
-    {
-    //  palSetPad(GPIOA, GPIOA_PIN13);     // Snur retning på motorer
-    //  palSetPad(GPIOA, GPIOA_PIN14);
-    //  palSetPad(GPIOC, GPIOC_PIN10);
-    //  palSetPad(GPIOC, GPIOC_PIN11);
-    //  palSetPad(GPIOC, GPIOC_PIN12);
+  
+  chThdCreateStatic(wa_manip_thread, MANIP_THREAD_STACK_SIZE, MANIP_THREAD_PRIORITY,
+                    manip_thread, NULL);
 
-      if(Joyakse1)
-      {
-        pwmEnableChannel(&PWMD1, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD1, 1000)); //PA8
-      }
-      else if(Joyakse2)
-      {
-        pwmEnableChannel(&PWMD2, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD2, 1000)); //PA15
-      }
-      else if(Joyakse3)
-      {
-        pwmEnableChannel(&PWMD3, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD3, 1000)); //PA15
-      }
-      else if(Joyakse4)
-      {
-        pwmEnableChannel(&PWMD4, 1, PWM_PERCENTAGE_TO_WIDTH(&PWMD4, 1000)); //PA15
-      }
-      else if(Joyakse5)
-      {
-        pwmEnableChannel(&PWMD8, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD8, 1000)); //PA15
-      }
-      
-       
-      // pwmChangePeriod(&PWMD4, 100);
-      // pwmChangePeriod(&PWMD4, 20);
-      // chThdSleepMilliseconds(3000);
-      // pwmDisableChannel(&PWMD4, 1);
-      // palClearPad(GPIOD, GPIOD_PIN3);
-      // pwmEnableChannel(&PWMD3, 2, PWM_PERCENTAGE_TO_WIDTH(&PWMD3, 1000));
-    }
-    pwmDisableChannel(&PWMD4,1);
+  while(TRUE){
+    
   }
 
   }
